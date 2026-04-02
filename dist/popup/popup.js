4e5ab0b656e4dcff2d23785873b6af076e2dae7d
Eucelia <Eucelia@noreply.github.com>
2026-04-01 23:38:38 -0700

    remove old files and add license

--- dist/popup/popup.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Popup script
-const openOptionsButton = document.getElementById('openOptions');
-openOptionsButton.addEventListener('click', () => {
-    chrome.runtime.openOptionsPage();
-});
--- src/popup/popup.ts
+++
@@ -1,6 +0,0 @@
-// Popup script
-
-const openOptionsButton = document.getElementById('openOptions') as HTMLButtonElement;
-openOptionsButton.addEventListener('click', () => {
-  chrome.runtime.openOptionsPage();
-});
